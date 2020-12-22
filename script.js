12cbbf435a42a5870fc587b7dabac8b195f80044
swapnil-shah <swapnilshah.me@gmail.com>
2020-12-22 14:10:56 -0500

    refactored prev and next clicks

--- script.js
+++ script.js
@@ -5,27 +5,30 @@ const datePicker = document.querySelector('.date-picker')
 const datePickerHeaderText = document.querySelector('.current-month')
 const previousMonthButon = document.querySelector('.prev-month-button')
 const nextMonthButon = document.querySelector('.next-month-button')
-
+let currentDate = new Date()
 datePickerButton.addEventListener("click", () => {
   datePicker.classList.toggle("show")
   const selectedDate = fromUnixTime(datePickerButton.dataset.selectedDate)
-  setUpDatePicker(selectedDate)
+  currentDate = selectedDate
+  setUpDatePicker()
 })
 
 function setDate(date) {
   datePickerButton.innerText = format(date, 'MMMM do, yyyy')
   datePickerButton.dataset.selectedDate = getUnixTime(date)
 }
-function setUpDatePicker(selectedDate) {
-  datePickerHeaderText.innerText = format(selectedDate, 'MMMM - yyyy');
-  setUpMonthButtons(selectedDate)
+function setUpDatePicker() {
+  datePickerHeaderText.innerText = format(currentDate, 'MMMM - yyyy')
+  setUpDates(selectedDate)
 }
-function setUpMonthButtons(selectedDate) {
-  previousMonthButon.addEventListener("click", () => {
-    setUpDatePicker(subMonths(selectedDate, 1))
-  }, { once: true })//Once true will completely remove listner and pmyl fire once otherwise it will add listerners over and over again
-  nextMonthButon.addEventListener("click", () => {
-    setUpDatePicker(addMonths(selectedDate, 1))
-  }, { once: true })
-}
+
+nextMonthButon.addEventListener("click", () => {
+  currentDate = addMonths(currentDate, 1)
+  setUpDatePicker()
+})
+
+previousMonthButon.addEventListener("click", () => {
+  currentDate = subMonths(currentDate, 1)
+  setUpDatePicker()
+})
 setDate(new Date())
